68af4ea5e4e80194f0c950cec24aa312965f9b06
vrcalpha <wacca1024@gmail.com>
2024-12-27 13:26:26 +0900

    要素の面積が小さいのを優先的に選択するようにした

--- chrome_extension/scripts/xpath_selector/OverlayManager.class.js
+++ chrome_extension/scripts/xpath_selector/OverlayManager.class.js
@@ -1,6 +1,6 @@
 import NodeFinder from "./NodeFinder.class.js";
 import StyleManager from "./StyleManager.class.js";
-import { htmlv, styleString } from "./htmlv.js";
+import { htmlv, styleString, dataset } from "./htmlv.js";
 
 class OverlayManager {
   // オーバーレイが表示されているか
@@ -9,6 +9,8 @@ class OverlayManager {
   #overlay = null;
   // 要素の情報表示用要素
   #info = null;
+  // 取得したノードたち
+  #foundNodes = null;
   
   constructor() {
     this.#loadStyle();
@@ -19,27 +21,15 @@ class OverlayManager {
     const titleKey = ['XPath', 'textContent', 'title', 'alt', 'placeholder'];
     const { overlay, info } = htmlv`
       <div class="vrc-overlay" *as="overlay"></div>
-      <div class="vrc-info" *as="info">
-        <div class="vrc-info__box">
-          <span class="vrc-info__title">XPath</span>
-          <textarea class="vrc-info__textarea" readonly>XPath</textarea>
-        </div>
-        <div class="vrc-info__box">
-          <span class="vrc-info__title">textContent</span>
-          <textarea class="vrc-info__textarea" readonly>textContent</textarea>
-        </div>
-        <div class="vrc-info__box">
-          <span class="vrc-info__title">title</span>
-          <textarea class="vrc-info__textarea" readonly>title</textarea>
-        </div>
-        <div class="vrc-info__box">
-          <span class="vrc-info__title">alt</span>
-          <textarea class="vrc-info__textarea" readonly>alt</textarea>
-        </div>
-        <div class="vrc-info__box">
-          <span class="vrc-info__title">placeholder</span>
-          <textarea class="vrc-info__textarea" readonly>placeholder</textarea>
-        </div>
+      <div class="vrc-info" *as="info[]">
+        ${repeat(titleKey, key => {
+          return /* html */`
+            <div class="vrc-info__box">
+              <span class="vrc-info__title">XPath</span>
+              <textarea class="vrc-info__textarea" readonly ${dataset({ key })} *as=""></textarea>
+            </div>
+          `;
+        })}
       </div>
     `;
     
@@ -70,20 +60,6 @@ class OverlayManager {
       .forEach(node => node.remove());
   }
   
-  #searchNodes() {
-    // 背景要素を表示したままだとelementFromPointが正常に機能しないため一旦隠す
-    this.#hide();
-    this.#reset();
-    
-    // テキストノードとtitleまたはplaceholder属性がついた要素を検索
-    const foundNodes = NodeFinder.findNodes(document.body);
-    console.log(foundNodes);
-    
-    // 検索結果をもとにオーバーレイ画面を作成
-    this.#setOverlay(foundNodes);
-    this.#show();
-  }
-  
   #setOverlay(nodes) {
     const fragment = document.createDocumentFragment();
     
@@ -92,21 +68,39 @@ class OverlayManager {
       // titleまたはplaceholder、alt属性を保持している場合は黄緑色、テキストのみを保持している場合は青色
       const color =
         (node.title || node.placeholder || node.alt) ? 'lime' : 'blue';
+      
       const style = styleString({
         left: node.x,
         top: node.y,
         width: node.width,
         height: node.height
       });
+      
+      const datasetIndex = dataset({ index });
         
       const popup = htmlv`
-        <div class="vrc-popup vrc-popup--${color}" style="${style}" data-index="${index}"></div>
+        <div class="vrc-popup vrc-popup--${color}" style="${style}" ${datasetIndex}></div>
       `;
       
       fragment.append(...popup);
     });
     
     this.#overlay.append(fragment);
+  }
+  
+  #searchNodes() {
+    // 背景要素を表示したままだとelementFromPointが正常に機能しないため一旦隠す
+    this.#hide();
+    this.#reset();
+    
+    // テキストノードとtitleまたはplaceholder属性がついた要素を検索
+    const foundNodes = NodeFinder.findNodes(document.body);
+    this.#foundNodes = foundNodes;
+    console.log(this.#foundNodes);
+    
+    // 検索結果をもとにオーバーレイ画面を作成
+    this.#setOverlay(this.#foundNodes);
+    this.#show();
   }
   
   #removeHighlight() {
@@ -142,11 +136,8 @@ class OverlayManager {
     if(!this.#isShow) return;
     this.#removeHighlight();
     
-    // カーソル上にある要素を取得
-    const target = document.elementFromPoint(e.clientX, e.clientY);
-    
-    // ポップアップのホバーのみ有効
-    if(!target.classList.contains('vrc-popup')) return;
+    const target = this.#getPopup(e);
+    if(target === null) return;
     
     this.#highlightOverlay(target);
   }
@@ -155,13 +146,48 @@ class OverlayManager {
     // 背景が表示されている間のみ有効
     if(!this.#isShow) return;
     
-    // カーソル上にある要素を取得
-    const hoverNodes = document.elementFromPoint(e.clientX, e.clientY);
+    const target = this.#getPopup(e);
+    if(target === null) return;
     
-    // ポップアップのクリックのみ有効
-    if(!hoverNodes.classList.contains('vrc-popup')) return;
+    console.log(target);
+    console.log(this.#info);
     
-    console.log(hoverNodes);
+    const id = target.dataset.index;
+    const property = this.#foundNodes[id];
+    
+    const keys = ['textContent', 'title', 'alt', 'placeholder'];
+    keys.forEach(key => {
+      this.#info[key].value = property[key];
+    });
+  }
+  
+  // カーソル座標上にある複数のポップアップのうち、一番大きさが小さいものを選ぶ
+  #getPopup(event) {
+    // カーソル上にある要素をすべて選択
+    const elements = document.elementsFromPoint(event.clientX, event.clientY);
+    
+    // ポップアップ要素のみ選択する
+    const filteredElements = elements.filter(element => element.classList.contains('vrc-popup'));
+    
+    if(filteredElements.length === 0) return null;
+    
+    // 各要素の面積を計算する
+    // TODO: 面積の計算をあらかじめキャッシュする
+    const areas = filteredElements.map(element => {
+      const client = element.getBoundingClientRect();
+      const { width, height } = client;
+      return {
+        target: element,
+        area: width * height
+      };
+    });
+    
+    // 面積が最小の要素を選択
+    const smallestElement = areas.reduce((min, current) => {
+      return (current.area < min.area) ? current : min;
+    });
+
+    return smallestElement.target;
   }
 }
 
