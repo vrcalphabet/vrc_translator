2963fe298528f917a77b6eddeb513c92ca6828a7
vrcalpha <wacca1024@gmail.com>
2025-01-05 14:18:21 +0900

    翻訳ルールの変更

--- chrome_extension/scripts/import.js
+++ chrome_extension/scripts/import.js
@@ -1,5 +1,5 @@
 import { initialize as initializeTranslate } from "./translate/translate.js";
 import { initialize as initializeSelector } from "./xpath_selector/selector.js";
 
-// initializeTranslate();
+initializeTranslate();
 initializeSelector();
